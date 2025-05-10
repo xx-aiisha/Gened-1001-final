0981e65ff514c08917fe571db25a776d3116ffb9
xx-aiisha <aisharashidahmed05@gmail.com>
2025-05-09 23:13:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let slides = document.querySelectorAll(".slide");
+let customSlides = document.querySelectorAll(".custom-slide");
+let allSlides = [...customSlides, ...slides, customSlides[1]];
+let current = 0;
+
+function showSlide(index) {
+    allSlides.forEach((slide, i) => {
+        slide.style.opacity = i === index ? 1 : 0;
+    });
+}
+
+function nextSlide() {
+    current = (current + 1) % allSlides.length;
+    showSlide(current);
+}
+
+showSlide(current);
+setInterval(nextSlide, 4000);
